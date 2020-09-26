05c469b2daaf6fb0c04781d19847c2936afe7f4f
Yu-Chen Chu <yuchenmichaelchu@gmail.com>
2020-09-26 16:07:56 -0400

    Added methodology to download JDK and install

--- Monitor_java.py
+++ Monitor_java.py
@@ -1,22 +1,35 @@
 import subprocess
 import re
 import os
+import requests
+import wget
+
+java_is_installed = True
 
 
 def check_java():
+    global java_is_installed
     try:
         version = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT)
-        version = version.decode("utf-8")
-        print(re.search(r'\"(\d+\.\d+).*\"', version).groups()[0])
+        decoded_version = version.decode("utf-8")
+        version_number = re.search(r'\"(\d+\.\d+).*\"', decoded_version).groups()[0]
+        print(version_number)
     except FileNotFoundError:
-
+        java_is_installed = False
         print("Java is not installed on the system")
-
     print("check_java_test")
+
+
+def install_java():
+    url = 'https://api.adoptopenjdk.net/v3/installer/latest/8/ga/windows/x64/jdk/hotspot/normal/adoptopenjdk'
+    filename = wget.download(url)
+    print(filename)
+    os.system('msiexec /i '+ filename + ' INSTALLLEVEL=1 /passive')
 
 
 def monitor_java():
     check_java()
-
+    install_java()
+    #check_java()
 
 monitor_java()
