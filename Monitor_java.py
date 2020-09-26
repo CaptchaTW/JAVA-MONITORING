a45e8cf9776fd58364478536d4fe79c9eb718d06
Yu-Chen Chu <yuchenmichaelchu@gmail.com>
2020-09-26 00:35:30 -0400

    check if java exist and version

--- Monitor_java.py
+++ Monitor_java.py
@@ -1,4 +1,22 @@
-import argparse
+import subprocess
+import re
+import os
 
-parser = argparse.ArgumentParser(description='Java Monitoring Commands')
 
+def check_java():
+    try:
+        version = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT)
+        version = version.decode("utf-8")
+        print(re.search(r'\"(\d+\.\d+).*\"', version).groups()[0])
+    except FileNotFoundError:
+
+        print("Java is not installed on the system")
+
+    print("check_java_test")
+
+
+def monitor_java():
+    check_java()
+
+
+monitor_java()
